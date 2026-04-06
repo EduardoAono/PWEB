144f2bd02c5b90becfc7cc90a3d4b830af891796
EduardoAono <eduardoeijii@gmail.com>
2026-04-06 12:02:42 -0300

    Add files via upload

---
+++ Atividade6/operacoes.js
@@ -0,0 +1,23 @@
+let continuar = confirm("Deseja realizar operações matemáticas?");
+
+if (continuar) {
+  let num1 = parseFloat(prompt("Digite o primeiro número:"));
+  let num2 = parseFloat(prompt("Digite o segundo número:"));
+
+  let soma = num1 + num2;
+  let subtracao = num1 - num2;
+  let produto = num1 * num2;
+  let divisao = num1 / num2;
+  let resto = num1 % num2;
+
+  alert(
+    "Resultados:\n" +
+    "Soma: " + soma + "\n" +
+    "Subtração: " + subtracao + "\n" +
+    "Produto: " + produto + "\n" +
+    "Divisão: " + divisao + "\n" +
+    "Resto: " + resto
+  );
+} else {
+  alert("Operação cancelada.");
+}
